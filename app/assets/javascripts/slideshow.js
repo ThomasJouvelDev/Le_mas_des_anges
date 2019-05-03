320753896f55c6286705936dbcba7c5e35ef53cc
Thomas Jouvel <thomas.jouvel@gmail.com>
2019-05-03 11:17:12 +0200

    design home page et bouton

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,18 +1,18 @@
-(function($) {
+// (function($) {
 
-  'use strict';
+//   'use strict';
 
-  var $slides = $('[data-slides]');
-  var images = $slides.data('slides');
-  var count = images.length;
-  var slideshow = function() {
-    $slides
-      .css('background-image', 'url("' + images[Math.floor(Math.random() * count)] + '")')
-      .show(0, function() {
-        setTimeout(slideshow, 5000);
-      });
-  };
+//   var $slides = $('[data-slides]');
+//   var images = $slides.data('slides');
+//   var count = images.length;
+//   var slideshow = function() {
+//     $slides
+//       .css('background-image', 'linear-gradient(-225deg, rgba(95, 95, 95, 0.3) 0%, rgba(0,0,0,0.3) 50%), url("' + images[Math.floor(Math.random() * count)] + '")')
+//       .show(0, function() {
+//         setTimeout(slideshow, 7000);
+//       });
+//   };
 
-  slideshow();
+//   slideshow();
 
-}(jQuery));
+// }(jQuery));
